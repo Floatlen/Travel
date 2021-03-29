aa38f87a65191f4a2453166d17ee651a06c60070
floatlen <floatlen@qq.com>
2021-03-29 16:11:38 +0800

    add commit

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div class='title'>周末去哪儿</div>
+    <ul>
+      <li
+        class='item border-bottom'
+        v-for='item of recommendList'
+        :key='item.id'
+      >
+        <div class='item-img-wrapper'>
+          <img class='item-img' :src='item.imgUrl' />
+        </div>
+        <div class='item-info'>
+          <p class='item-title'>{{ item.title }}</p>
+          <p class='item-desc'>{{ item.desc }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl:
+            '//imgs.qunarzz.com/p/p70/1809/e7/4941057a6aae702.jpg_256x160_9fee6ccb.jpg',
+          title: '丽江',
+          desc: '4天自由行'
+        },
+        {
+          id: '0002',
+          imgUrl:
+            '//imgs.qunarzz.com/p/p14/201302/28/218ac6605f1d637f93835fbb.jpg_256x160_bfee6c2d.jpg',
+          title: '桂林',
+          desc: '往返火车票+3晚住宿'
+        },
+        {
+          id: '0003',
+          imgUrl:
+            '//imgs.qunarzz.com/p/p67/1512/a2/0ebfcd965b9391f7.jpg_256x160_04d5813d.jpg',
+          title: '三亚',
+          desc: '5天跟团游'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow hidden
+    height 0
+    padding-bottom 42.2%
+    .item-img
+      width 100%
+  .item-info
+    padding .1rem
+  .item-title
+    line-height: .54rem
+    font-size: .32rem
+    ellipsis()
+  .item-desc
+    line-height: .4rem
+    color: #ccc
+    ellipsis()
+</style>
---
+++ src/pages/home/components/recommend.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <div class='title'>热销推荐</div>
+    <ul>
+      <li
+        class='item border-bottom'
+        v-for='item of recommendList'
+        :key='item.id'
+      >
+        <img class='item-img' :src='item.imgUrl' />
+        <div class='item-info'>
+          <p class='item-title'>{{ item.title }}</p>
+          <p class='item-desc'>{{ item.desc }}</p>
+          <button class='item-button'>查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl:
+            '//img1.qunarzz.com/sight/p0/1605/58/5847560fd55ff6f590.water.jpg_256x160_13631f57.jpg',
+          title: '凤凰古城',
+          desc: '邂逅数个人，偶遇一座城'
+        },
+        {
+          id: '0002',
+          imgUrl:
+            '//img1.qunarzz.com/sight/p0/2011/1b/1b5e5d4a4003bbc3a3.img.jpg_256x160_2448993c.jpg',
+          title: '张家界国家森林公园',
+          desc: '三千奇峰，八百秀水'
+        },
+        {
+          id: '0003',
+          imgUrl:
+            '//img1.qunarzz.com/sight/p0/1412/e2/650fc27b271e57e2b8ea952aa3e48eea.water.jpg_256x160_45707f83.jpg',
+          title: '衡山风景区',
+          desc: '华山如立，嵩山如卧，惟有南岳衡山独如飞'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .16rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+        color: #fff
+</style>

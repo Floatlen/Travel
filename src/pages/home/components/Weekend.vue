b339670672cf1954ab47af1296c334ea87880b96
floatlen <floatlen@qq.com>
2021-03-29 16:54:09 +0800

    index finish

--- src/pages/home/components/Weekend.vue
+++ src/pages/home/components/Weekend.vue
@@ -4,7 +4,7 @@
     <ul>
       <li
         class='item border-bottom'
-        v-for='item of recommendList'
+        v-for='item of list'
         :key='item.id'
       >
         <div class='item-img-wrapper'>
@@ -21,48 +21,23 @@
 
 <script>
 export default {
-  name: 'HomeWeekend',
-  data () {
-    return {
-      recommendList: [
-        {
-          id: '0001',
-          imgUrl:
-            '//imgs.qunarzz.com/p/p70/1809/e7/4941057a6aae702.jpg_256x160_9fee6ccb.jpg',
-          title: '丽江',
-          desc: '4天自由行'
-        },
-        {
-          id: '0002',
-          imgUrl:
-            '//imgs.qunarzz.com/p/p14/201302/28/218ac6605f1d637f93835fbb.jpg_256x160_bfee6c2d.jpg',
-          title: '桂林',
-          desc: '往返火车票+3晚住宿'
-        },
-        {
-          id: '0003',
-          imgUrl:
-            '//imgs.qunarzz.com/p/p67/1512/a2/0ebfcd965b9391f7.jpg_256x160_04d5813d.jpg',
-          title: '三亚',
-          desc: '5天跟团游'
-        }
-      ]
-    }
-  }
+  props: {
+    list: Array
+  },
+  name: 'HomeWeekend'
 }
 </script>
 
 <style lang='stylus' scoped>
   @import '~styles/mixins.styl'
   .title
-    margin-top: .2rem
     line-height: .8rem
     background: #eee
     text-indent: .2rem
   .item-img-wrapper
     overflow hidden
     height 0
-    padding-bottom 42.2%
+    padding-bottom 37.09%
     .item-img
       width 100%
   .item-info
